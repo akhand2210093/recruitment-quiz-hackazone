67b1231077397a6db150ee58bccb57beb2f5ba51
akhand2210093 <akhandpratapsingh11c@gmail.com>
2024-08-22 16:08:22 +0530

    updates

--- quiz_website/quiz/views.py
+++ quiz_website/quiz/views.py
@@ -71,6 +71,8 @@ class QuestionListView(generics.ListAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
+    #just to commit and make 
+
 # Submit all responses at once
 class SubmitResponseView(APIView):
     permission_classes = [IsAuthenticated]
